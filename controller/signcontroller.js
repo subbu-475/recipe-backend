6ceaca4febb9ba76df8cbbe3b5042ac7670d30bf
subbu-475 <subbudme475@gmail.com.com>
2023-12-02 11:29:54 +0530

    for verify token update

--- controller/signcontroller.js
+++ controller/signcontroller.js
@@ -22,7 +22,7 @@ const InsertVerifyUser = async(name,email,password)=>{
         userVerify.save().then(()=>{
             console.log("user saved in the userverify collections");
         })
-        const activationLink=`http://localhost:8000/signin/${token}`
+        const activationLink=`https://subbu-recipe-book.onrender.com/${token}`
         const content = `<h4>Hi,there</h4>
         <p>Plese click the below link to verify</p>
         <a href=${activationLink}>click here</a>
